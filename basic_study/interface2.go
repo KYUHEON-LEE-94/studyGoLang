3a7b4b9a57049bf8d9680c4eabc666e7e1b0f43b
khlee <panpan68@naver.com>
2024-01-30 10:03:37 +0900

    interface

---
+++ basic_study/interface2.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x interface{}
+	x = 1
+	x = "Tom"
+
+	printIt(x)
+
+	var a interface{} = 1
+
+	i := a       // a와 i 는 dynamic type, 값은 1
+	j := a.(int) // j는 int 타입, 값은 1
+
+	println(a)
+	println(i) // 포인터주소 출력
+	println(j) // 1 출력
+}
+
+func printIt(v interface{}) {
+	fmt.Println(v) //Tom
+}
